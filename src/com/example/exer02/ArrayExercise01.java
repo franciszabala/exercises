36499068bd87c808e1a9afb71833beb13baa2ce4
francis.zabala <francis.zabala@yahoo.com>
2015-04-17 15:48:54 +0800

    -added new array exercise

---
+++ src/com/example/exer02/ArrayExercise01.java
@@ -0,0 +1,13 @@
+package com.example.exer02;
+
+public class ArrayExercise01 {
+
+	public static void main (String args[]) {
+		/** Create an array of integers **/
+		
+		/** Output them in the order **/
+		
+		/** Output them in reverse **/
+	}
+	
+}
