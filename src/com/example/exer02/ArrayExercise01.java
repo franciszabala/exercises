205bc4797b40c2c7f2100953239d9bb17b90c71a
wenax <arwen80700@gmail.com>
2015-04-18 16:20:55 +0400

    added answer

--- src/com/example/exer02/ArrayExercise01.java
+++ src/com/example/exer02/ArrayExercise01.java
@@ -1,13 +1,33 @@
 package com.example.exer02;
 
 public class ArrayExercise01 {
+    
+	public static int[] reverse(int[] array) {
+		    for(int counter=0; counter<array.length/2; counter++) {
+		        int temp = array[counter];                    
+		        array[counter] = array[array.length-1-counter];
+		        array[array.length-1-counter] = temp;
+		    }
+		    return array;
+		}
 
-	public static void main (String args[]) {
-		/** Create an array of integers **/
+		public static void main(String[] args) {
+			/** Create an array of integers **/
+			
+			/** Output them in the order **/
+			
+			/** Output them in reverse **/
+		    int[] array = {2,4,6,8};
+		    int[] newArray = reverse(array);
+		    for(int counter=0; counter<newArray.length; counter++) {          
+		        System.out.print(newArray[counter]+" ");      
+		    }
+
 		
-		/** Output them in the order **/
+		}
+
 		
-		/** Output them in reverse **/
+
 	}
 	
-}
+
