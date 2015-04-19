83346c3defda6d5acc776a32dae6b9a6a3e87c9a
wenax <arwen80700@gmail.com>
2015-04-19 10:24:55 +0400

    multiplication table answer

--- src/allan.java
+++ src/allan.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
 
 public class allan {
+	public static void main (String []args){
+		Scanner num= new Scanner (System.in);
+		int table[]= {1,2,3,4,5,6,7,8,9,10};
+		System.out.println ("multiplication table:  ");
+		int temp = num.nextInt();
+				
+			for (int counter = 0; counter < table.length; counter++){
+				int answer = table[counter]*temp;
+				
+			}
+				
+			for(int counter2 =0; counter2 < table.length; counter2++){
+				int answer = table[counter2]*temp;
+					
+					System.out.println (table [counter2] + "x" + temp + "=" + answer);
+					
+			}
+				
 
+}
 }
